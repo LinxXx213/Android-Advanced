c4466d33fe88be20867d36789fd1ea71bad5d96f
LinxXx213 <132820182+LinxXx213@users.noreply.github.com>
2023-07-15 03:17:01 -0500

    Delete FCMnotificationPractice directory

--- FCMnotificationPractice/app/src/main/java/com/pandit/application/fcmnotificationpractice/models/DealsDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.pandit.application.fcmnotificationpractice.models;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {Deal.class}, version = 1)
-public abstract class DealsDatabase extends RoomDatabase {
-    public abstract DealsDAO dealsDAO();
-}
